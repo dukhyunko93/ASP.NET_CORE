5077784c8af5f05dd0c2a25da8ec3da7caf3421d
Duke Ko <dukhyunko93@gmail.com>
2021-02-17 11:37:57 -0500

    Array and 2D Array

---
+++ Udemy C++/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int array1[] = {1,2,3};
+
+    string array2[5];
+    array2[1] = "Hello";
+
+    cout<<array1[0]<<endl;
+    cout<<array2[1];
+
+    return 0;
+}
