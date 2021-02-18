dfaee28f884db4a4d32b652fd524ead0b14cddd2
Duke Ko <dukhyunko93@gmail.com>
2021-02-18 11:01:06 -0500

    Class

---
+++ Udemy C#/Class/Class/Math/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Class.Math
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Udemy C#/Class/Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class.Math;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            var result = Calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
