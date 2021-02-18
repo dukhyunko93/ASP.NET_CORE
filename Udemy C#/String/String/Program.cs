01e852e003ad5f989c31eaed4c01dd385f718bd8
Duke Ko <dukhyunko93@gmail.com>
2021-02-18 13:13:04 -0500

    C# String

---
+++ Udemy C#/String/String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var firstName = "Duke";
+            var lastName = "Ko";
+
+            var fullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var fornattedNames = string.Join(",", names);
+            Console.WriteLine(fornattedNames);
+
+            var text = @"Hi John
+Look into the following paths
+C:\folder1\folder2\folder2";
+
+            Console.WriteLine(text);
+        }
+    }
+}
