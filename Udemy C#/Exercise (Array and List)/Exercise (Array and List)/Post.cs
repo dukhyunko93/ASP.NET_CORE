084a5d87eff2ee4115ddae94ee3b5fbc02907a3a
Duke Ko <dukhyunko93@gmail.com>
2021-02-23 18:14:03 -0500

    first question finished

---
+++ Udemy C#/Exercise (Array and List)/Exercise (Array and List)/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise__Array_and_List_
+{
+    public class Post
+    {
+        public List<string> likers = new List<string>();
+    }
+}
---
+++ Udemy C#/Exercise (Array and List)/Exercise (Array and List)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise__Array_and_List_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stop = false;
+            var post = new Post();
+            Console.WriteLine(post);
+
+            while (!stop)
+            {
+                Console.WriteLine("Input a name: ");
+                string name = Console.ReadLine();
+                if (name == "")
+                {
+                    stop = true;
+                }
+                else
+                {   
+                    post.likers.Add(name);
+                }
+            }
+            if (post.likers.Count == 1)
+            {
+                Console.WriteLine(post.likers[0] + " likes your post");
+            }
+            else if (post.likers.Count == 2)
+            {
+                Console.WriteLine(post.likers[0] + " and " + post.likers[1] + " likes your post");
+            }
+            else if(post.likers.Count > 2)
+            {
+                var other = post.likers.Count - 2;
+                Console.WriteLine(post.likers[0] + ", " + post.likers[1] + " and " + other + " likes your post");
+            }
+        }
+    }
+}
