8d219e5635ff64ddefb69415c60d3bd6c58e9fb1
Duke Ko <dukhyunko93@gmail.com>
2021-02-24 09:58:59 -0500

    Annotations in TS

---
+++ TypeScript/annotations/variables.ts
@@ -0,0 +1,10 @@
+let apple: number = 5;
+apple = 10;
+// apple = 'wadaw';
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+
+//built in object
+let now: Date = new Date();
